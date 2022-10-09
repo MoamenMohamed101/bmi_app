fb7394a4565d065338d238fc22cd138c4dad5a43
Moamen Mohamed <99367343+MoamenMohamed101@users.noreply.github.com>
2022-10-09 02:28:07 +0200

    login_screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,10 +1,21 @@
 import 'package:bmi_app/shared/components/components.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailController = TextEditingController();
+
   TextEditingController passwordController = TextEditingController();
+
+  var formKey = GlobalKey<FormState>();
+
+  bool? isPassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -14,63 +25,68 @@ class LoginScreen extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Login',
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email Address',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.email),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
                   ),
-                  keyboardType: TextInputType.emailAddress,
-                  onFieldSubmitted: (value) {
-                    print(value);
-                  },
-                  onChanged: (value) {
-                    print(value);
-                  },
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: Icon(Icons.remove_red_eye),
+                  const SizedBox(
+                    height: 40,
                   ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: true,
-                  onFieldSubmitted: (value) {
-                    print(value);
-                  },
-                  onChanged: (value) {
-                    print(value);
-                  },
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                dufultButton(
+                  defaultFormField(
+                    controller: emailController,
+                    keyboard: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'enter email';
+                      }
+                      return null;
+                    },
+                    text: 'Enter email address',
+                    prefixIcon: Icons.email,
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  defaultFormField(
+                    controller: passwordController,
+                    keyboard: TextInputType.visiblePassword,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'enter password';
+                      }
+                      return null;
+                    },
+                    text: 'Enter your password',
+                    prefixIcon: Icons.lock,
+                    suffixIcon: Icons.add,
+                    isPassword: isPassword!,
+                    iconSuffix: () {
+                      setState(() {
+                        isPassword = !isPassword!;
+                      });
+                    },
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  defaultButton(
                     color: Colors.blue,
                     text: 'login',
                     voidCallback: () {
-                      print(emailController.text);
+                      if (formKey.currentState!.validate())
+                        print(emailController.text);
                       print(passwordController.text);
                     },
-                    width: double.infinity)
-              ],
+                    width: double.infinity,
+                    isUpperCase: true,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-dufultButton(
-        {double? width,
-        Color? color,
-        VoidCallback? voidCallback,
-        String? text}) =>
+defaultButton({
+  double? width,
+  Color? color = Colors.blue,
+  @required VoidCallback? voidCallback,
+  @required String? text,
+  bool? isUpperCase = true,
+  double? radius = 10.0,
+}) =>
     Container(
       width: width,
       decoration: BoxDecoration(
         color: color,
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(radius!),
       ),
       child: MaterialButton(
         onPressed: voidCallback,
         child: Text(
-          text!.toUpperCase(),
+          isUpperCase! ? text!.toUpperCase() : text!,
           style: const TextStyle(color: Colors.white, fontSize: 20),
         ),
       ),
     );
+
+defaultFormField(
+        {@required TextEditingController? controller,
+        @required TextInputType? keyboard,
+        void Function(String)? onFieldSubmitted,
+        void Function(String)? onChanged,
+        @required String? Function(String?)? validator,
+        @required String? text,
+        @required IconData? prefixIcon,
+        IconData? suffixIcon,
+        bool isPassword = false,
+        VoidCallback? iconSuffix}) =>
+    TextFormField(
+      validator: validator,
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: text!,
+        border: const OutlineInputBorder(),
+        prefixIcon: Icon(prefixIcon!),
+        suffixIcon: suffixIcon != null
+            ? IconButton(
+                onPressed: iconSuffix,
+                icon: isPassword
+                    ? const Icon(Icons.visibility_off)
+                    : const Icon(Icons.remove_red_eye),
+              )
+            : null,
+      ),
+      keyboardType: keyboard,
+      obscureText: isPassword,
+      onFieldSubmitted: onFieldSubmitted,
+      onChanged: onChanged,
+    );
+//
